2d550975a28d27b355c57b3ac73e4c4b4468e857
1-mochi <ez.editz.cc@gmail.com>
2026-04-07 18:57:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const WebSocket = require('ws');
 
-// IMPORTANT: use Render's port
 const port = process.env.PORT || 8080;
-
-// Create WebSocket server
 const wss = new WebSocket.Server({ port });
 
 console.log(`Server running on port ${port}`);
@@ -11,18 +8,16 @@ console.log(`Server running on port ${port}`);
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
-    ws.on('message', (message) => {
-        console.log('Received:', message.toString());
+    // Send the test message immediately on connection
+    const testMessage = {
+        auth: "supersecret123",
+        name: "Cigno Fulgoro,Snailo Clovero,Granny",
+        generation: "200,111,27",
+        players: "7/8",
+        jobid: "af713949-3dc6-47ee-bd80-0c19e672b6ba"
+    };
 
-        // Send reply back
-        ws.send('Hello from server!');
-    });
+    ws.send(JSON.stringify(testMessage));
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-
-    ws.on('error', (err) => {
-        console.error('Error:', err);
-    });
+    ws.on('close', () => console.log('Client disconnected'));
 });
